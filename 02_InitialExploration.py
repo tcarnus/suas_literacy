9edb40375dd2caadc596803ce479e2ffd6fb6ffd
jonsedar <jon.sedar@applied.ai>
2014-07-21 01:28:34 +0100

    got pymc3 working for basic regression

--- 02_InitialExploration.py
+++ 02_InitialExploration.py
@@ -44,6 +44,8 @@ import scipy as sp
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import sqlite3
+from sklearn import linear_model
+import pymc as pm
 
 #from sklearn.covariance import MinCovDet
 #from sklearn.preprocessing import Normalizer
@@ -106,6 +108,176 @@ df.head()
 
 # <codecell>
 
+## scores pre vs post for all
+
+x = df['staard_score_pre']
+y = df['staard_score_post']
+
+cm_cmap = cm.get_cmap('hsv')
+fig, axes1d = plt.subplots(nrows=1, ncols=1, sharex=True, figsize=(12,6))
+fig.subplots_adjust(wspace=0.2)
+fig.suptitle('Correlation of pre-test and post-test scores')
+
+sp = axes1d
+clr = cm_cmap(0)
+sp.scatter(x=x,y=y,color=clr,alpha=0.5,edgecolor='#999999')
+
+# fit and plot a new linear model
+regr = linear_model.LinearRegression()
+regr.fit(pd.DataFrame(x),pd.DataFrame(y))
+x_prime = np.linspace(x.min(),x.max(),num=10)[:,np.newaxis]
+y_hat = regr.predict(x_prime)
+sp.plot(x_prime,y_hat,linewidth=2,linestyle='dashed',color='green',alpha=0.8)
+
+ss_res = regr.residues_[0]
+ss_tot = np.sum((y - np.mean(y))**2)
+rsq = 1 - (ss_res/ss_tot)
+
+sp.annotate('R^2:  {:.2f}\nCoef: {:.2f}\nIntr: {:.2f}'.format(rsq,regr.coef_[0][0],regr.intercept_[0])
+                ,xy=(1,0),xycoords='axes fraction'
+                ,xytext=(-12,6),textcoords='offset points',color='green',weight='bold'
+                ,size=12,ha='right',va='bottom')
+sp.set_ylabel('Post-test Score')
+sp.set_xlabel('Pre-test Score')
+sp.axes.grid(True,linestyle='-',color='lightgrey')
+
+plt.subplots_adjust(top=0.85)
+plt.show()  
+
+# <codecell>
+
+## scores pre vs post for test type
+
+## TODO
+
+# <markdowncell>
+
+# ---
+
+# <headingcell level=1>
+
+# Bayesian Regression
+
+# <markdowncell>
+
+# ## Unpooled approach 
+# ... for comparison with Frequentist
+
+# <codecell>
+
+## try unpooled model first for all
+
+x = df['staard_score_pre']
+y = df['staard_score_post']
+
+with pm.Model() as individual_model:
+
+    # priors for intercept, slope and precision - all uninformative
+    alpha = pm.Normal('alpha', mu=0, sd=100**2)
+    beta = pm.Normal('beta', mu=0, sd=100**2)
+    sigma = pm.Uniform('sigma', lower=0, upper=100)
+
+    # Linear model
+    y_est = alpha + beta * x
+
+    # Data likelihood
+    likelihood = pm.Normal('event like', mu=y_est, sd=sigma, observed=y)
+
+    # keep trace
+    traces = pm.sample(5000, step=pm.NUTS(), start=pm.find_MAP(), progressbar=True)
+
+    pm.traceplot(traces)
+
+# <codecell>
+
+## quick plot of regression
+
+def plot_reg(sp, alpha, beta, sigma, xlims, maxlike=False):  
+    x = np.arange(xlims[0], xlims[1])
+    y_est = eval('{} + {}*x'.format(alpha, beta))
+    if maxlike:    
+        sp.plot(x, y_est, linewidth=3, linestyle='dashed', color='darkorange', alpha=0.8)
+        sp.annotate('alpha: {:.2f}\nbeta:  {:.2f}\nsigma: {:.2f}'.format(alpha, beta, sigma)
+                ,xy=(1,0),xycoords='axes fraction',xytext=(-12,6),textcoords='offset points'
+                ,color='darkorange',weight='bold',size=12,ha='right',va='bottom')
+    else:
+        sp.plot(x, y_est, color='#333333', alpha=0.05)
+
+cm_cmap = cm.get_cmap('hsv')
+fig, axes1d = plt.subplots(nrows=1, ncols=1, sharex=True, figsize=(12,6))
+fig.subplots_adjust(wspace=0.2)
+fig.suptitle('Correlation of pre-test and post-test scores - Bayesian')
+
+sp = axes1d
+clr = cm_cmap(0)
+sp.scatter(x=x,y=y,color=clr,alpha=0.5,edgecolor='#999999')
+plot_reg(sp, traces['alpha'].mean(), traces['beta'].mean(), traces['sigma'].mean()
+         ,xlims=[x.min(),x.max()],maxlike=True)  
+
+for i in xrange(1000,5000,50):
+    point = traces.point(i)
+    plot_reg(sp, point['alpha'], point['beta'], point['sigma'], xlims=[x.min(),x.max()])
+
+#plt.show()
+
+# <codecell>
+
+## try unpooled model first for test type
+
+unqvals_tests = np.unique(df.index.get_level_values('test').tolist())
+traces_unpooled = {}
+
+for testtype in unqvals_tests:
+
+    x = df.query('test=="{}"'.format(testtype))['staard_score_pre']
+    y = df.query('test=="{}"'.format(testtype))['staard_score_post']
+    
+    with pm.Model() as individual_model:
+        
+        # intercept prior (variance == sd**2) and slope prior
+        alpha = pm.Normal('alpha', mu=0, sd=100**2)
+        beta = pm.Normal('beta', mu=0, sd=100**2)
+    
+        # Model error prior
+        eps = pm.Uniform('eps', lower=0, upper=100)
+    
+        # Linear model
+        y_est = alpha + beta * x
+    
+        # Data likelihood
+        likelihood = pm.Normal('event like', mu=y_est, sd=eps, observed=y)
+        
+        # keep trace
+        traces_unpooled[testtype] = pm.sample(2000, step=pm.NUTS(), start=pm.find_MAP(), progressbar=True)
+        
+        # this is more robust to outliers you need stats model installed
+        # with pm.Model() as model_robust:
+        #    family = pm.glm.families.T()
+  
+
+# <codecell>
+
+for testtype in unqvals_tests:
+    print('Estimates for: {}'.format(testtype))
+    tp = pm.traceplot(traces_unpooled[testtype])
+    tp.show()
+
+# <codecell>
+
+def plot_breg(formula, x_min, x_max):  
+    x = np.arange(x_min, x_max)
+    y_est = eval(formula)
+    plt.plot(x, y_est)  
+    
+plt.scatter(x,y)
+plot_breg('23 + 0.79*x', x.min(), x.max())
+plt.show()
+
+# <codecell>
+
+
+# <codecell>
+
 
 # <codecell>
 
@@ -128,7 +300,4 @@ df.head()
 # 
 # _copyleft_ **Open Data Science Ireland Meetup Group 2014**  
 # <a href='http://www.meetup.com/opendatascienceireland/'>ODSI@meetup.com</a>
-
-# <codecell>
-
 
